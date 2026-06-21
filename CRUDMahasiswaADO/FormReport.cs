8e90f5e1929ce3b418dc5d302c1813b5ee6d3b81
tari2025 <denilestari272@gmail.com>
2026-06-21 19:37:16 +0700

    merevisi kode supaya jalan

--- CRUDMahasiswaADO/FormReport.cs
+++ CRUDMahasiswaADO/FormReport.cs
@@ -3,6 +3,8 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Windows.Forms;
 using CrystalDecisions.CrystalReports.Engine;
+using OfficeOpenXml;
+using System.IO;
 
 namespace CRUDMahasiswaADO
 {
--- CRUDMahasiswaADO/dsMahasiswa.cs
+++ CRUDMahasiswaADO/dsMahasiswa.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using System;
+using OfficeOpenXml;
+using System.IO;
 
 namespace PrakMahasiswaADO
 {
